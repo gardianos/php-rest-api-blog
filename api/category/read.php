e51690d74d8c230e22fa66f24ba3bc3acde10725
Gardian <gardianimehmeti@gmail.com>
2020-03-14 02:34:37 +0100

    Initial commit

---
+++ api/category/read.php
@@ -0,0 +1,49 @@
+<?php
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+//Instantiate DB & connect 
+$database = new Database();
+$db = $database->connect();
+
+//Instatiate blog category object 
+$category = new Category($db);
+
+//Blog post query
+$result = $category->read();
+//Get row count 
+$num = $result->rowCount();
+
+//Check if any categories 
+if($num > 0){
+    //Category array
+$cat_arr = array();
+$cat_arr['data']= array();
+
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+extract($row);
+
+$cat_item = array(
+    'id'=> $id,
+    'name'=>$name
+    
+);
+    // Push to the data 
+    array_push($cat_arr['data'], $cat_item);
+}
+    // Turn to JSON and output 
+
+    echo json_encode($cat_arr);
+
+}else{
+    //No categories 
+    echo json_encode(array(
+        'message'=>'No category found'
+    ));
+
+}
+?>
